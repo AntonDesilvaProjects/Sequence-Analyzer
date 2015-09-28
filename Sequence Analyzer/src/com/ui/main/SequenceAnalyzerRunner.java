047f901e84e2e9f497e1f670b47db9ed2ff3a713
Anton Desilva <antondesilva09@yahoo.com>
2015-09-28 12:46:27 -0400

    First Test commit

--- Sequence Analyzer/src/com/ui/main/SequenceAnalyzerRunner.java
+++ Sequence Analyzer/src/com/ui/main/SequenceAnalyzerRunner.java
@@ -5,10 +5,10 @@ import com.data.local.MutationDAL;
 public class SequenceAnalyzerRunner {
 
 	public static void main(String[] args) {
-		//frmMain main = new frmMain();
+		frmMain main = new frmMain();
 		
 		MutationDAL dal = new MutationDAL();
-		java.util.List l = dal.generateProteinSequences("C:\\Users\\Anton\\Desktop\\mutations.txt");
+		//java.util.List l = dal.generateProteinSequences("C:\\Users\\Anton\\Desktop\\mutations.txt");
 		
 	}
 
